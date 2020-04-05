dd9bffab6f048eaaa14149abd79dbcc7ad06e189
Wojciech Pierwoła <wpierwola@gmail.com>
2020-04-05 16:35:07 +0200

    Files placed in a folder

---
+++ web_app/test_main.py
@@ -0,0 +1,21 @@
+
+from fastapi.testclient import TestClient
+import pytest
+from main import app
+
+client = TestClient(app)
+
+
+def test_read_main():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"message": "Hello World during the coronavirus pandemic!"}
+
+
+@pytest.mark.parametrize("name", ['Zenek', 'Marek', 'Alojzy Niezdąży'])
+def test_hello_name(name):
+    # name = 'elo'
+    response = client.get(f"/hello/{name}")
+    assert response.status_code == 200
+    assert response.json() == {'msg': f"Hello {name}"}
+
