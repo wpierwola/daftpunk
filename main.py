33e72d27286e97e3c438a911ca1d3359d7a7279b
Wojciech Pierwoła <wpierwola@gmail.com>
2020-04-05 18:25:28 +0200

    added example 2

--- main.py
+++ main.py
@@ -4,19 +4,28 @@ from fastapi import FastAPI
 
 from pydantic import BaseModel
 
-
 app = FastAPI()
-
 
 @app.get("/")
 def root():
     return {"message": "Hello World during the coronavirus pandemic!"}
 
 
-class HelloResp(BaseModel):
-    msg: str
+@app.get("/method/")
+def get_method():
+    return {"method": "GET"}
 
 
-@app.get("/hello/{name}", response_model=HelloResp)
-def read_item(name: str):
-    return HelloResp(msg=f"Hello {name}")
+@app.post("/method/")
+def get_method():
+    return {"method": "POST"}
+
+
+@app.put("/method/")
+def get_method():
+    return {"method": "PUT"}
+
+
+@app.delete("/method/")
+def get_method():
+    return {"method": "DELETE"}
--- test_main.py
+++ test_main.py
@@ -12,10 +12,28 @@ def test_read_main():
     assert response.json() == {"message": "Hello World during the coronavirus pandemic!"}
 
 
-@pytest.mark.parametrize("name", ['Zenek', 'Marek', 'Alojzy Niezdąży'])
-def test_hello_name(name):
-    # name = 'elo'
-    response = client.get(f"/hello/{name}")
+def test_get_method():
+    response = client.get("/method/")
     assert response.status_code == 200
-    assert response.json() == {'msg': f"Hello {name}"}
+    assert response.json() == {"method": "GET"}
+
+
+def test_post_method():
+    response = client.post("/method/")
+    assert response.status_code == 200
+    assert response.json() == {"method": "POST"}
+
+
+def test_put_method():
+    response = client.put("/method/")
+    assert response.status_code == 200
+    assert response.json() == {"method": "PUT"}
+
+
+def test_delete_method():
+    response = client.delete("/method/")
+    assert response.status_code == 200
+    assert response.json() == {"method": "DELETE"}
+
+
 
