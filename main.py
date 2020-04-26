6bf7327b7a194810e3c156e26929b5de676702a7
Wojciech Pierwoła <wpierwola@gmail.com>
2020-04-26 15:35:05 +0200

    fixed login system

--- main.py
+++ main.py
@@ -78,23 +78,18 @@ def pk_patient(pk: int):
     else:
         raise HTTPException(status_code=204, detail="no_content")
 
-@app.get("/login")
-#@app.post("/login/")
+
+@app.post("/login/")
 def auth_login(credentials: HTTPBasicCredentials = Depends(security)):
     correct_username = secrets.compare_digest(credentials.username, "trudnY")
     correct_password = secrets.compare_digest(credentials.password, "PaC13Nt")
-    if not correct_username and correct_password:
+    if not (correct_username and correct_password):
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail="Incorrect credentials",
             headers={"WWW-Authenticate": "Basic"},
         )
-    session_token = sha256(bytes(f"{credentials.username}{credentials.password}{app.secret_key}")).hexdigest()
+    session_token = sha256(bytes(f"{credentials.username}{credentials.password}{app.secret_key}", encoding = "utf8")).hexdigest()
     response = RedirectResponse(url="/welcome/")
     response.set_cookie(key="session_token", value=session_token)
     return response
-
-
-
-
-
